b5361a4038f80dfce8a3100869f840088f1ffa35
kKondratek <k.kondratek@o2.pl>
2020-03-28 13:57:40 +0100

    test: add missing tests

--- src/test/java/com/kondratek/forum/ForumManagerTest.java
+++ src/test/java/com/kondratek/forum/ForumManagerTest.java
@@ -2,7 +2,6 @@ package com.kondratek.forum;
 
 import com.kondratek.JsonReader;
 import com.kondratek.forum.db.UserNotFoundException;
-import com.kondratek.forum.db.model.Post;
 import com.kondratek.forum.db.model.User;
 import org.junit.Assert;
 import org.junit.Before;
@@ -94,8 +93,59 @@ public class ForumManagerTest {
         // exception thrown
     }
 
-    // todo genNonUniqueTitles test
+    @Test
+    public void getNonUniqueTitles_singlePost() throws IOException {
+        // given
+        String postJson = getPostJson(1, 1, "title 1");
+        String postJsonArray = getObjectArrayJson(postJson);
+        PowerMockito.when(JsonReader.fetchJsonString(TEST_URL)).thenReturn(postJsonArray);
 
+        // when
+        Set<String> nonUniqueTitles = forumManager.getNonUniqueTitles();
+
+        // then
+        Assert.assertTrue(nonUniqueTitles.isEmpty());
+    }
+
+    @Test
+    public void getNonUniqueTitles_twoEqualTitles() throws IOException, UserNotFoundException {
+        // given
+        final String title = "title 1";
+        String user = getUserJson(1);
+        String userJsonArray = getObjectArrayJson(user);
+        String postJson1 = getPostJson(1, 1, title);
+        String postJson2 = getPostJson(1, 2, title);
+        String postJsonArray = getObjectArrayJson(postJson1, postJson2);
+        PowerMockito.when(JsonReader.fetchJsonString(TEST_URL)).thenReturn(userJsonArray, postJsonArray);
+
+        // when
+        forumManager.fetchUsersFromURL(TEST_URL);
+        forumManager.fetchUsersPostsFromUrl(TEST_URL);
+        Set<String> nonUniqueTitles = forumManager.getNonUniqueTitles();
+
+        // then
+        Assert.assertEquals(1, nonUniqueTitles.size());
+        Assert.assertTrue(nonUniqueTitles.contains(title));
+    }
+
+    @Test
+    public void getNonUniqueTitles_twoUniqueTitles() throws IOException, UserNotFoundException {
+        // given
+        String user = getUserJson(1);
+        String userJsonArray = getObjectArrayJson(user);
+        String postJson1 = getPostJson(1, 1, "title 1");
+        String postJson2 = getPostJson(1, 2, "title 2");
+        String postJsonArray = getObjectArrayJson(postJson1, postJson2);
+        PowerMockito.when(JsonReader.fetchJsonString(TEST_URL)).thenReturn(userJsonArray, postJsonArray);
+
+        // when
+        forumManager.fetchUsersFromURL(TEST_URL);
+        forumManager.fetchUsersPostsFromUrl(TEST_URL);
+        Set<String> nonUniqueTitles = forumManager.getNonUniqueTitles();
+
+        // then
+        Assert.assertTrue(nonUniqueTitles.isEmpty());
+    }
 
     private String getObjectArrayJson(String... jsonObjects) {
         return "[" + String.join(", ", jsonObjects) + "]";
@@ -107,5 +157,9 @@ public class ForumManagerTest {
 
     private String getPostJson(int userId, int id) {
         return String.format("{ \"userId\": %d, \"id\": %d, \"title\": \"\", \"body\": \"\" }", userId, id);
+    }
+
+    private String getPostJson(int userId, int id, String title) {
+        return String.format("{ \"userId\": %d, \"id\": %d, \"title\": %s, \"body\": \"\" }", userId, id, title);
     }
 }
